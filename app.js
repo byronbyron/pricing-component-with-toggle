44b0d5b944c1ac5d9f45e44b16eadc41b3704c07
Byronn Walker-Mills <bwalkermills@gmail.com>
2022-03-22 22:07:04 +0000

    Handle price toggle js

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const toggle = document.getElementById('pricing');
+const anually = document.querySelectorAll('.anually');
+const monthly = document.querySelectorAll('.monthly');
 
 toggle.addEventListener('change', handleToggle);
 
 function handleToggle(e) {
   if (e.target.checked) {
-    console.log('monthly');
+    console.log(anually)
+    anually.forEach(element => element.setAttribute('hidden', true));
+    monthly.forEach(element => element.removeAttribute('hidden'));
   } else {
-    console.log('anually');
+    anually.forEach(element => element.removeAttribute('hidden'));
+    monthly.forEach(element => element.setAttribute('hidden', true));
   }
 }
